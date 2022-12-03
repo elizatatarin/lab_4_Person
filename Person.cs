bbc0d91d2526d2838b903e4d11ba8e57150788b1
eliza <eliza@DESKTOP-HLLIIPJ>
2022-12-04 01:04:36 +0200

    Add project files.

---
+++ Person.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторна_4
+{
+    internal class Person
+    {
+        private string name;
+        private int birthYear;
+        private DateTime date = DateTime.Today;
+
+        public string Name
+        {
+            get { return name; }
+        }
+
+        public int BirthYear
+        {
+            get { return birthYear; }
+        }
+
+        //Конструктор за замовчуванням
+        public Person(): this("Eliza", 2004)
+        { }
+
+        //Конструктор з двома параметрами
+        public Person(string name, int birthYear)
+        {
+            this.name = name;
+            this.birthYear = birthYear;
+        }
+
+        //Методи
+
+        //Обчислення віку особи
+        public void Age()
+        {
+            birthYear = date.Year - birthYear;
+            Console.WriteLine(birthYear);
+        }
+
+        //Виведення даних про особу з консолі
+        public void Input()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine("Введіть ім'я:");
+            this.name = Console.ReadLine();
+            Console.WriteLine("Введіть рік народження:");
+            this.birthYear = Convert.ToInt32(Console.ReadLine());
+        }
+
+        //Змінити ім'я особи
+        public void ChangeName(string newName)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            this.name = newName;
+            Console.WriteLine($"Ім'я було змінено на: {newName}");
+        }
+
+        //Вивести інформацію про особу
+        public void Output()
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            Console.WriteLine($"Ім'я: {this.name}, Вік: {this.birthYear}");
+        }
+
+        //ToString
+        public override string ToString()
+        {
+            return $"Ім'я: {this.name}, Вік: {this.birthYear}";
+        }
+        
+        //Оператор (рівний за іменем)
+        public static bool operator == (Person per1, Person per2)
+        {
+            return (per1.name == per2.name);
+        }
+        public static bool operator != (Person per1, Person per2)
+        {
+            return (per1.name != per2.name);
+        }
+
+        //Виведення інформації про осіб з однаковими іменами
+        public void Comparsion(Person per1, Person per2)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            Console.InputEncoding = System.Text.Encoding.Unicode;
+
+            if (per1 == per2)
+            {
+                Console.WriteLine($"Люди з однаковими іменами та інформація про них: {per1}; {per2}");
+            }
+            else
+            {
+                Console.WriteLine($"В осіб різні імена");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Лабораторна_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Натиснiть будь-яку кнопку, для того, щоб вивести данi на екран");
+            Console.ReadKey();
+
+            //Person
+            Person per1 = new Person("Eliza", 2004);
+            Person per2 = new Person("Inna", 1978);
+            Person per3 = new Person("Maks", 2019);
+            Person per4 = new Person("Eliza", 1995);
+            Person per5 = new Person("Olexandr", 2001);
+            Person per6 = new Person("Anastasia", 2008);
+
+            per1.Age();
+            per2.Age();
+            per3.Age();
+            per4.Age();
+            per5.Age();
+            per6.Age();
+
+            per1.Output();
+            per2.Output();
+            per3.Output();
+            per4.Output();
+            per5.Output();
+            per6.Output();
+
+            per3.ChangeName("Very Young");
+            per3.Output();
+
+            per6.ChangeName("Very Young");
+            per6.Output();
+
+            Console.WriteLine($"Інформація про всіх: {per1}; {per2}; {per3}; {per4}; {per5}; {per6}");
+
+            per1.Comparsion(per1, per4);
+            per1.Comparsion(per1, per6);
+
+            Console.WriteLine("Натисніть будь-яку кнопку, для того, щоб закрити консоль");
+            Console.ReadKey();
+        }
+    }
+}
